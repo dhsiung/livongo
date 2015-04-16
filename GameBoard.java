3620ab6888ff364785b8b3664c569a5c2d4f812d
Daniel Hsiung <hsiung.daniel@gmail.com>
2015-04-16 00:19:51 -0700

    added check_win. still needs to properly implement player switching and check tie

--- GameBoard.java
+++ GameBoard.java
@@ -15,7 +15,6 @@ public class GameBoard extends Cell{
 	}
 	public GameBoard(int row, int col) {
 		// cells = new Cell[ROWS][COLS];
-
 		for(row = 0; row< ROWS; ++row) {
 			for (col= 0; col < COLS; ++col){
 				cells[row][col] = new Cell(row, col);
@@ -44,11 +43,35 @@ public class GameBoard extends Cell{
     }
 
     public boolean check_win (Marker player) {
+    	// 3 in a row like -
+    	System.out.println("1: "+cells[currRow][0].content);
+    	System.out.println("player: " + player);
+    	if (cells[currRow][0].content == player &&
+    		cells[currRow][1].content == player &&
+    		cells[currRow][2].content == player) {
+    		return true;
+    	}
+    	// 3 in a col like |
+    	else if ( cells[0][currCol].content == player &&
+	    		cells[1][currCol].content == player &&
+	    		cells[2][currCol].content == player) {
+    		return true;
+    	}
+    	// 3 diagonally like \
+    	else if (currRow == currCol && 
+	    		cells[0][0].content == player &&
+	    		cells[1][1].content == player &&  
+	    		cells[2][2].content == player) {
+    		return true;
+    	}
+    	// 3 diagonally like /
+    	else if (currRow + currCol == 2 && 
+	    		cells[0][2].content == player &&
+	    		cells[1][1].content == player &&  
+	    		cells[2][0].content == player) {
+    		return true;
+    	}
+    
     	return false;
     }
-	
-	// public static void main (String[] args){
-
-	// 	GameBoard board = new GameBoard(ROWS,COLS);
-	// }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -11,7 +11,7 @@ public class TicTacToe extends GameBoard {
 	public static final int ROWS = 3;
 	public static final int COLS = 3;
 	private GameStatus currentStatus = GameStatus.PLAYING;
-	private Marker currentPlayer; // X is human, O is circle, human always goes first
+	private Marker currentPlayer = Marker.CROSS; // X is human, O is circle, human always goes first
 	private GameBoard board;
 	private static Scanner in = new Scanner(System.in);
 
@@ -26,28 +26,42 @@ int currRow, currCol = 0;
 			board.drawBoard();
 			playerInput();
 			aiMove();
-			board.drawBoard();
+			// board.drawBoard();
+			updateGameStatus(currentPlayer);
+			if (currentStatus == GameStatus.PLAYER_WIN) {
+				System.out.println("You have beaten my AI!");
+			}
+			else if (currentStatus == GameStatus.AI_WIN) {
+				System.out.println("Only a fool would think they could beat me!");
+			}
+			else if (currentStatus == GameStatus.TIE) {
+				System.out.println("It's a tie!");
+			}
 		} while (currentStatus == GameStatus.PLAYING); 
 	}
 
 	/* unfinished, hardcoded to find cell position. fix later! */
+	// todo implement check if cell is already taken
 
 	public void playerInput() {
 		// int currRow, currCol = 0;
-		System.out.println("Where to? ");
+		System.out.print("Where to? ");
 		int input = in.nextInt();
+		String cellPosition="";
 		switch (input) {
-			case 1: currRow = 0; currCol=0; break;
-			case 2: currRow = 0; currCol=1; break;
-			case 3: currRow = 0; currCol=2; break;
-			case 4: currRow = 1; currCol=0; break;
-			case 5: currRow = 1; currCol=1; break;
-			case 6: currRow = 1; currCol=2; break;
-			case 7: currRow = 2; currCol=0; break;
-			case 8: currRow = 2; currCol=1; break; 
-			case 9: currRow = 2; currCol=2; break;
+			case 1: currRow = 0; currCol=0; cellPosition = "upper left"; break;
+			case 2: currRow = 0; currCol=1; cellPosition = "upper middle"; break;
+			case 3: currRow = 0; currCol=2; cellPosition = "upper right"; break;
+			case 4: currRow = 1; currCol=0; cellPosition = "middle left"; break;
+			case 5: currRow = 1; currCol=1; cellPosition = "middle"; break;
+			case 6: currRow = 1; currCol=2; cellPosition = "middle right"; break;
+			case 7: currRow = 2; currCol=0; cellPosition = "lower left"; break;
+			case 8: currRow = 2; currCol=1; cellPosition = "lower middle"; break; 
+			case 9: currRow = 2; currCol=2; cellPosition = "lower right"; break;
 		}
 		board.cells[currRow][currCol].content = Marker.CROSS;
+		System.out.println("You have put an X in the " + cellPosition + ".");
+		System.out.println("currRow: " + currRow + "  currcol: " + currCol);
 		// board.col = currCol;
 		// currCol = (input - COLS) % COLS;
 		// System.out.println(currCol);
@@ -56,7 +70,10 @@ int currRow, currCol = 0;
 	public void updateGameStatus(Marker player) {
 		if ( board.check_win(player) ) {
 			currentStatus = (player == Marker.CROSS) ? GameStatus.PLAYER_WIN : GameStatus.AI_WIN;
+			System.out.println("HIT THE LOOP!");
 		}
+		System.out.println(board.check_win(player));
+		// System.out.println("testest!");
 		// else if (board.)
 	}
 
